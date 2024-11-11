07551a49ef5ceb3b6e8ebd46e2703694ffefab94
Escalante Pablo <141648314+adamblade12@users.noreply.github.com>
2024-11-11 09:21:21 -0300

    subir tp4

---
+++ tp4/Poblar tablas.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO Ordenes (Id, ClienteId, FechaOrden, Estado)
+VALUES
+    (1, 101, '2022-01-01', 'Pendiente'),
+    (2, 102, '2022-01-05', 'Procesando'),
+    (3, 101, '2022-01-10', 'Enviado'),
+    (4, 103, '2022-01-15', 'Cancelada'),
+    (5, 102, '2022-01-20', 'Entregada'),
+    (6, 101, '2022-01-25', 'Pendiente'),
+    (7, 103, '2022-02-01', 'Procesando'),
+    (8, 102, '2022-02-05', 'Enviado'),
+    (9, 101, '2022-02-10', 'Cancelada'),
+    (10, 103, '2022-02-15', 'Entregada');
