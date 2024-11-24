e28a9554b1192adb1e7114a818c75a45f9abfb84
Escalante Pablo <141648314+adamblade12@users.noreply.github.com>
2024-11-24 20:42:34 -0300

    Correcciones

--- tp4/Poblar tablas.sql
+++ tp4/Poblar tablas.sql
@@ -1,13 +1,13 @@
 
-INSERT INTO Ordenes (Id, ClienteId, FechaOrden, Estado)
+INSERT INTO ordenes (ClienteId, FechaOrden, Estado,Total)
 VALUES
-    (1, 101, '2022-01-01', 'Pendiente'),
-    (2, 102, '2022-01-05', 'Procesando'),
-    (3, 101, '2022-01-10', 'Enviado'),
-    (4, 103, '2022-01-15', 'Cancelada'),
-    (5, 102, '2022-01-20', 'Entregada'),
-    (6, 101, '2022-01-25', 'Pendiente'),
-    (7, 103, '2022-02-01', 'Procesando'),
-    (8, 102, '2022-02-05', 'Enviado'),
-    (9, 101, '2022-02-10', 'Cancelada'),
-    (10, 103, '2022-02-15', 'Entregada');
+    (101, '2022-01-01', 'Pendiente',1),
+    (102, '2022-01-05', 'Procesando',2),
+    (101, '2022-01-10', 'Enviado',3),
+    (103, '2022-01-15', 'Cancelada',4),
+    (102, '2022-01-20', 'Entregada',5),
+    (101, '2022-01-25', 'Pendiente',6),
+    (103, '2022-02-01', 'Procesando',7),
+    (102, '2022-02-05', 'Enviado',8),
+    (101, '2022-02-10', 'Cancelada',9),
+    (103, '2022-02-15', 'Entregada',10);
